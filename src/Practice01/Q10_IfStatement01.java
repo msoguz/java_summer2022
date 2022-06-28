43106dad436514e8698aec8745ba87eb58e11907
M. Sener OGUZ <mseneroguz@gmail.com>
2022-06-28 16:14:24 +0300

    28.06.2022 eklemeleri

---
+++ src/Practice01/Q10_IfStatement01.java
@@ -0,0 +1,30 @@
+package Practice01;
+
+import java.util.Scanner;
+
+public class Q10_IfStatement01 {
+    public static void main(String[] args) {
+        /*
+         * KullanicidanY/N ikilisinden birisini girdiginde girdigi degeri
+         * ekrana yazdiran java kodunu yaziniz.
+         * INPUT : Y ,  N
+         * OUTPUT : YES ; NO
+         */
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Lutfen y/n ikilisinden birini giriniz :");
+
+        char karakter=scan.next().charAt(0);
+
+        if (karakter=='Y' || karakter=='y') {
+            System.out.println("YES");
+        }else if (karakter=='N' || karakter=='n') {
+        }else{
+            System.out.println("yanlis giris yaptiniz, lutfen y/n ikilisinden birini tercih ediniz");
+
+        }
+
+
+    }
+}
