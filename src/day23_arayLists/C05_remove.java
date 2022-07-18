abfd57ff4b2f973a8539a8a31d9e6f9cb17da676
M. Sener OGUZ <mseneroguz@gmail.com>
2022-07-19 01:21:33 +0300

    19.07.2022.2

--- src/day23_arayLists/C05_remove.java
+++ src/day23_arayLists/C05_remove.java
@@ -11,6 +11,7 @@ public class C05_remove {
         sayilar1.add(5);
         sayilar1.add(3);
         sayilar1.add(2);
+        sayilar1.add(1);
 
         System.out.println(sayilar1); // [5, 3, 2, 1]
         sayilar1.remove(1);
@@ -22,16 +23,15 @@ public class C05_remove {
 
         System.out.println(sayilar1); // [5, 2, 1]
 
-        // illa da 5'i silsin dersek
-        // silinecek objeyi bir variable'a tanimlayabiliriz
-
+        // illa da 5'i silsin dersek silinecek objeyi bir variable'a tanimlayabiliriz
         Integer sil=5;
+
         sayilar1.remove(sil);
         System.out.println(sayilar1); // [2, 1]
 
         // veya indexOf method'undan yardim alabilirim
-
         sayilar1.remove(sayilar1.indexOf(2));
+
         // 2 yi silmek icin 2'nin index'ini bulup, onu remove'a yazabiliriz
         System.out.println(sayilar1); // [1]
 
